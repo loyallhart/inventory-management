2a85108b7cc58a23f20ca652dd42d7a162c2f3ea
Blue Valenz <bluevalenz@Blues-MacBook-Pro.local>
2023-05-02 19:05:57 -0400

    fixed html

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ function loginUser() {
       alert("Please enter your password");
       return;
     }
-    fetch("/login", {
-      method: "POST",
-      body: JSON.stringify({ username: username, password: password }),
-      headers: { "Content-Type": "application/json" }
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      if (data.success) {
-        window.location.href = "/dashboard";
-      } else {
-        alert(data.message);
-      }
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-  }
+  //   fetch("/login", {
+  //     method: "POST",
+  //     body: JSON.stringify({ username: username, password: password }),
+  //     headers: { "Content-Type": "application/json" }
+  //   })
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(data) {
+  //     if (data.success) {
+  //       window.location.href = "/dashboard";
+  //     } else {
+  //       alert(data.message);
+  //     }
+  //   })
+  //   .catch(function(error) {
+  //     console.error(error);
+  //   });
+  // }
