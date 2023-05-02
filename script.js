dc50b4d56ccebf6eea2b8eff175d1aa5bab17eca
Blue Valenz <bluevalenz@Blues-MacBook-Pro.local>
2023-05-01 21:09:12 -0400

    js script fixing

--- script.js
+++ script.js
@@ -11,24 +11,4 @@ function loginUser() {
       alert("Please enter your password");
       return;
     }
-  
-    fetch("/login", {
-      method: "POST",
-      body: JSON.stringify({ username: username, password: password }),
-      headers: { "Content-Type": "application/json" }
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      if (data.success) {
-        window.location.href = "/dashboard";
-      } else {
-        alert(data.message);
-      }
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-  }
-  
+}...
