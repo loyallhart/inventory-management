44fe101a25f843498c0582a7941f89a92f628627
nabeekha <nabeekha@umich.edu>
2023-05-02 20:36:36 -0400

    updated function to seed the db files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,21 @@
 const sequelize = require('../config/connection')
-const { Products } = require('..models')
-
+const { Product } = require('..models')
 const productSeedData = require('./productSeedData.json')
+const categorySeedData = [
+    { name: 'Total Products/Inventory' },
+    { name: 'Low Stock Products' },
+    { name: 'Out of Stock Products' },
+    { name: 'Damaged Inventory' },
+    { name: 'Products Deployed Internally' },
+  ]
 
 //seed db function
 const seedDB = async () => {
     await sequelize.sync({force: true})
+    const products = await Product.bulkCreate(productSeedData)
+    const categories = await Category.bulkCreate(categorySeedData);
 
-    const products = await Products.bulkCreate(productSeedData)
-
-    ProcessingInstruction.exit(0)
+    console.log(`Seeded ${products.length} products and ${categories.length} categories`);
 }
 
 seedDB()
