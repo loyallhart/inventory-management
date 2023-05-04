6923f3950e06419fc3e67d65624206b6306d5c5a
Blue Valenz <bluevalenz@Blues-MacBook-Pro.local>
2023-05-04 17:02:18 -0400

    js login

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
-function loginUser() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+// function loginUser() {
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
   
-    if (username.trim() == "") {
-      alert("Please enter your username");
-      return;
-    }
-    if (password.trim() == "") {
-      alert("Please enter your password");
-      return;
-    }
-    fetch("/login", {
-      method: "POST",
-      body: JSON.stringify({ username: username, password: password }),
-      headers: { "Content-Type": "application/json" }
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      if (data.success) {
-        window.location.href = "/dashboard";
-      } else {
-        alert(data.message);
+    const loginUser= async (event) => {
+      event.preventDefault();
+    
+      const username = document.querySelector('#username-login').value.trim();
+      const password = document.querySelector('#password-login').value.trim();
+
+      if (username && password) {
+
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify({ username, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert(response.statusText);
+        }
       }
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-  }
+    };
+
+    document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginUser);
+
+    // if (username.trim() == "") {
+    //   alert("Please enter your username");
+    //   return;
+    // }
+    // if (password.trim() == "") {
+    //   alert("Please enter your password");
+    //   return;
+    // }
+    // fetch("/login", {
+    //   method: "POST",
+    //   body: JSON.stringify({ username: username, password: password }),
+    //   headers: { "Content-Type": "application/json" }
+    // })
+    // .then(function(response) {
+    //   return response.json();
+    // })
+    // .then(function(data) {
+    //   if (data.success) {
+    //     window.location.href = "/dashboard";
+    //   } else {
+    //     alert(data.message);
+    //   }
+    // })
+    
+  
 
   function s() {
     document.querySelector('.notification').classList.remove('hidden');
