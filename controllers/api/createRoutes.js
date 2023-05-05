b4d535cb3b4204909a9239767fc65a6e0339240e
Dewey Watkins <deweywatkins77@gmail.com>
2023-05-04 20:27:11 -0400

    updated create routes to reflect database table names and column names

--- controllers/api/createRoutes.js
+++ controllers/api/createRoutes.js
@@ -1,18 +1,37 @@
 const router = require('express').Router();
-const { items } = require('../../models');
+const { Products, Categories } = require('../../models');
 
-router.put('/item', async (req, res) => {
+router.post('/product', async (req, res) => {
     try {
-      // const data = req.body
-      // items.create({
-      //     name:data.name,
-      //     quantity:data.quantity
-      //     etc
-      // })
-  
+      const data = req.body
+      await Products.create({
+        name: data.name,
+        model: data.model,
+        manufacturer: data.manufacturer,
+        purchaseDate: data.purchase.date,
+        quantity: data.quantity,
+        status: data.status,
+        owner: data.owner,
+        categoryId: data.category
+      })
+      res.sendStatus(200)
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
+      console.log(err)
     }
+});
+
+router.post('/category', async (req, res) => {
+  try {
+    const data = req.body
+    await Categories.create({
+      name: req.body.name,
+    })
+    res.sendStatus(200)
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err)
+  }
 });
 
 module.exports = router
