795068caa7d77444f9aca1f8c7e8cd2549755e35
Dewey Watkins <deweywatkins77@gmail.com>
2023-05-06 19:41:05 -0400

    homeroutes for products and categories created. currently just returns an object

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,20 +3,20 @@ const { Product, Category } = require('../models');
 
 router.get('/products', async (req, res) => {
     try {
-      let products = await Product.findall()
+      let products = await Product.findAll()
       products = products.map(product => product.get({plain:true}))
-      res.sendStatus(200).json({products})
+      res.status(200).json({products})
     } catch (err) {
       res.status(500).json(err);
       console.log(err)
     }
 });
 
-router.post('/categories', async (req, res) => {
+router.get('/categories', async (req, res) => {
   try {
     let categories = await Category.findAll()
     categories = categories.map(categories => categories.get({plain:true}))
-    res.sendStatus(200).json({categories})
+    res.status(200).json({categories})
   } catch (err) {
     res.status(500).json(err);
     console.log(err)
