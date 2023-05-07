4164510fa0082b4965751fca1a3e13dd91359e6f
nabeekha <nabeekha@umich.edu>
2023-05-07 16:45:16 -0400

    encrypted passwords with bcrypt

--- models/users.js
+++ models/users.js
@@ -1,32 +1,62 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt')
 
-class Users extends Model {}
+class Users extends Model {
+    async setPassword(password) {
+        const rounds = 10;
+        const hashedPassword = await bcrypt.hash(password, rounds);
+        this.password = hashedPassword;
+      }
+    
+      // compare entered password with hashed password in database
+      async checkPassword(password) {
+        const match = await bcrypt.compare(password, this.password);
+        return match;
+      }
+}
 
 Users.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true,
+          },
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [8],
+          },
+        },
       },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'users'
-  }
-);
-
+      {
+        hooks: {
+          beforeCreate: async (newUserData) => {
+            await newUserData.setPassword(newUserData.password);
+          },
+          beforeUpdate: async (updatedUserData) => {
+            if (updatedUserData.password) {
+              await updatedUserData.setPassword(updatedUserData.password);
+            }
+          },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+      }
+    );
+    
 module.exports = Users
